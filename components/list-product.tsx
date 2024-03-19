a436a0bff610f0e890d9fdcc93796aa07d3dd5da
byeoru <byeoru215@gmail.com>
2024-03-19 19:44:19 +0900

    product list prefetch test

--- components/list-product.tsx
+++ components/list-product.tsx
@@ -18,7 +18,7 @@ export default function ListProduct({
   created_at,
 }: ListProductProps) {
   return (
-    <Link href={`/products/${id}`} className="flex gap-5">
+    <Link href={`/products/${id}`} className="flex gap-5" prefetch>
       <div className="relative size-28 rounded-md overflow-hidden">
         <Image
           fill
